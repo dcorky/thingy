c15b26ccc76dd0b619fd423549b94bc54aca1d71
dcorky <djocorky@gmail.com>
2019-06-22 00:05:07 -0500

    Initial commit; more to follow

---
+++ shout.py
@@ -0,0 +1,16 @@
+def shout():
+    phrase = input("What was that? ")
+    if phrase == phrase.upper():
+        print("WHY ARE YOU SHOUTING?")
+    else:
+        phrase = input("Can you speak up? ")
+        if phrase == phrase.upper():
+            print("WHY ARE YOU SHOUTING?")
+        else:
+            phrase = input("I still can't hear you. ")
+            if phrase == phrase.upper():
+                print("WHY ARE YOU SHOUTING?")
+            else:
+                print("""You know what, this isn't working.
+Sorry.""")
+                
