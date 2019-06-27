18b4e6cb6bd79c3454a13466a3ee8f4fd112d667
dcorky <djocorky@gmail.com>
2019-06-27 12:41:25 -0500

    Updated beginning and response kinda

--- shout.py
+++ shout.py
@@ -1,16 +1,24 @@
 def shout():
-    phrase = input("What was that? ")
+    phrase = input("""What was that?
+""")
     if phrase == phrase.upper():
         print("WHY ARE YOU SHOUTING?")
     else:
-        phrase = input("Can you speak up? ")
+        phrase = input("""Can you speak up?
+""")
         if phrase == phrase.upper():
             print("WHY ARE YOU SHOUTING?")
         else:
-            phrase = input("I still can't hear you. ")
+            phrase = input("""I still can't hear you.
+""")
             if phrase == phrase.upper():
                 print("WHY ARE YOU SHOUTING?")
             else:
                 print("""You know what, this isn't working.
-Sorry.""")
+Sorry.
+Bye.""")
                 
+response = input("""Hello! How are you?
+""")
+if response != response.upper():
+    shout()
